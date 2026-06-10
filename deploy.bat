0539df4542c33bc9f999e2b01c28cde55a0694f0
David Kubíček <service@hartje-factory.cz>
2026-06-10 11:23:07 +0200

    Automatický deploy

--- deploy.bat
+++ deploy.bat
@@ -1,21 +1,27 @@
 @echo off
+chcp 65001 >nul
+color 0A
 echo ============================================
 echo   QiO-2027 - Automatický GitHub deploy
 echo ============================================
 
 echo.
-echo 1️⃣ Ukládám změny...
+echo [1] Ukládám změny...
 git add .
 git commit -m "Automatický deploy"
 
 echo.
-echo 2️⃣ Odesílám na GitHub...
+color 0E
+echo [2] Odesílám na GitHub...
 git push
 
 echo.
-echo 3️⃣ Generuji a nasazuji web...
+color 0B
+echo [3] Generuji a nasazuji web...
 mkdocs gh-deploy
 
 echo.
+color 0A
 echo ✅ Hotovo! Web bude dostupný za pár sekund.
 pause
+
--- docs/modules/Zadek-kabel3x3/Zadek-kabel3x3.md
+++ docs/modules/Zadek-kabel3x3/Zadek-kabel3x3.md
@@ -1,7 +1,4 @@
-<img src="{{ base_url }}/modules/Zadek-kabel3x3/images/zadek-3x3.png" width="70%">
-
-
-<img src="{{ base_url }}/modules/Zadek-kabel3x3/images/zadek2-3x3.png" width="70%">
+<img src="{{ base_url }}/modules/Zadek-kabel3x3/images/zadek.png" width="70%">
 
 
 Kabel zadního světla se provleče mezi patkou rámu (slider) a adaptérem na brzdu, aby se zajistilo, že se kabel neponičí o kotouč. Kabel musí být i tak dostatečně napnutý
